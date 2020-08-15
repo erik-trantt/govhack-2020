1efc6165efa585b454d35d6d09cdcf2b36ec8f55
Erik <erik.tran.tt@gmail.com>
2020-08-15 15:10:35 +1000

    implement chat display for bot and user

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,15 +4,23 @@ class MessagesController < ApplicationController
     @message = Message.new(message_params)
     @message.chatroom = @chatroom
     @message.user = current_or_guest_user
+    bot_message = reply_from_chatbot(@message)
 
-    if @message.save
+    if @message.save && bot_message.save
       ChatroomChannel.broadcast_to(
         @chatroom,
-        render_to_string(partial: "message", locals: { message: @message })
+        render_to_string(partial: "messages/message-right", locals: { message: @message })
       )
+
+      ChatroomChannel.broadcast_to(
+        @chatroom,
+        render_to_string(partial: "messages/message-left", locals: { message: bot_message })
+      )
+
       redirect_to chatroom_path(@chatroom, anchor: "message-#{@message.id}")
     else
-      render "chatrooms/show"
+      flash[:alert] = @message.errors
+    #   render "chatrooms/show"
     end
   end
 
@@ -20,5 +28,13 @@ class MessagesController < ApplicationController
 
   def message_params
     params.require(:message).permit(:content)
+  end
+
+  def reply_from_chatbot(message)
+    text = 'hello there'
+    bot_reply = Message.new(content: text)
+    bot_reply.chatroom = message.chatroom
+    bot_reply.user = User.find_by(username: 'bot')
+    bot_reply
   end
 end
