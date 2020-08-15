31b49eb9f0966d7ed1c795ea9ed5cb67834696f2
Erik <erik.tran.tt@gmail.com>
2020-08-16 00:41:59 +1000

    added route /chat and move logic to there

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   resources :chatrooms, only: :show do
     resources :messages, only: :create
   end
-
+  get '/chat', to: 'pages#show'
   post '/messages', to: 'messages#parse'
 end
