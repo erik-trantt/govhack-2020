f2ea2ad8b67740a2b013a75087934c778ded886c
Erik <erik.tran.tt@gmail.com>
2020-08-15 12:33:06 +1000

    setup: guest user can now send message to app

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -26,9 +26,11 @@ require("channels")
 import "bootstrap";
 
 // Internal imports, e.g:
+import "../channels/index";
+import { initChatroomCable } from "../channels/chatroom_channel";
 // import { initSelect2 } from '../components/init_select2';
 
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
-  // initSelect2();
+  initChatroomCable();
 });
